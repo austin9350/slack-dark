79e26b14f0c0489d75309f46ccc45fe70da6a1b9
Austin Younts <austin.younts@bmwgroup.com>
2018-09-25 13:40:32 -0400

    Update css url

--- loadCss.js
+++ loadCss.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
  $.ajax({
-   url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
+   url: 'https://gist.githubusercontent.com/austin9350/2c1dd619f7f6360598e9550ac2f9a4e1/raw/38d449d0fe64e0204116e5f4188446673c291441/dark-mode.css',
    success: function(css) {
      $("<style></style>").appendTo('head').html(css);
      $("<style></style>").appendTo('head').html('#reply_container.upload_in_threads .inline_message_input_container {background: padding-box #545454}');
